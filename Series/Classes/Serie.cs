c2a124ff6345d5f338432a7d3ac4bd8cb7099df4
Celina Takeda <takedacarina@gmail.com>
2022-01-31 12:15:09 -0300

    commit

--- Series/Classes/Serie.cs
+++ Series/Classes/Serie.cs
@@ -2,8 +2,50 @@ namespace Series
 {
     public class Serie : EntidadeBase
     {
+        // Atributos
 
-    }
-        
-    
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+        private bool Excluido { get; set; }
+
+        // Métodos
+
+        public Serie(int id, Genero genero, String titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+            this.Excluido = false;
+        }
+
+        public override string ToString()
+        {
+            // Environment.NewLine https://docs.microsoft.com/en-us/dotnet/api/system.environment.newline?view=netcore-3.1
+            string retorno = "";
+            retorno += "Genero: " + this.Genero + Environment.NewLine;
+            retorno += "Titulo: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de Início: " + this.Ano + Environment.NewLine;
+            return retorno;
+        }
+
+        public string retornaTitulo()
+        {
+            return this.Titulo;
+        }
+
+        public int retornaId()
+        {
+            return this.Id;
+        }
+
+        public void Excluir()
+        {
+            this.Excluido = true;
+        }
+    }    
 }
